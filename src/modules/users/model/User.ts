23797d9ffc2b472f238bbdb2312fa27a74e1caba
Daniel R Gorgonha <rogergorgonha@gmail.com>
2021-03-24 11:53:17 -0300

    Added userRepository functions and performed all user creation tests

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,22 +1,33 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  id: string;
+  id?: string;
 
   name: string;
 
-  admin: boolean;
+  admin?: boolean;
 
   email: string;
 
-  created_at: Date;
+  created_at?: Date;
 
-  updated_at: Date;
+  updated_at?: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
+    }
+
+    if (!this.admin) {
       this.admin = false;
+    }
+
+    if (!this.created_at) {
+      this.created_at = new Date();
+    }
+
+    if (!this.updated_at) {
+      this.updated_at = new Date();
     }
   }
 }
